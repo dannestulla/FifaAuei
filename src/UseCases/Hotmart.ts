2c19ea1bddeeda8992a45c3c52a67254b307e8b2
dannestulla <dannestulla@gmail.com>
2023-06-04 22:50:33 -0300

    removed currency check

--- src/UseCases/Hotmart.ts
+++ src/UseCases/Hotmart.ts
@@ -55,12 +55,12 @@ export class HotmartUseCase {
 
   static isForeignCurrency = (items: Item[]): boolean => {
     let isForeign = false
-     for (const itemSelected in items) {
+    /*  for (const itemSelected in items) {
       const itemTyped = (itemSelected as any) as Item
       if (itemTyped.purchase.price.currency_code != "BRL") {
         isForeign = true
       }
-    } 
+    }  */
     return isForeign
   }
 }
